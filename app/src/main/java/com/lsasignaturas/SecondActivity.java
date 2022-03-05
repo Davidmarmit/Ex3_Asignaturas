8786930bb71b80a0c946164c99c9e3c2a4677f4e
David <davidmartinezmit@gmail.com>
2022-03-05 18:36:40 +0100

    App funcional

--- app/src/main/java/com/lsasignaturas/SecondActivity.java
+++ app/src/main/java/com/lsasignaturas/SecondActivity.java
@@ -1,14 +1,15 @@
 package com.lsasignaturas;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 public class SecondActivity extends AppCompatActivity {
     TextView[] text = new TextView[6];
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -23,21 +24,40 @@ public class SecondActivity extends AppCompatActivity {
 
         switch (temp){
             case 0:
-
+                text[0].setText(R.string.a1);
+                text[1].setText(R.string.a1_professor);
+                text[2].setText(R.string.a1_detail);
+                text[3].setText(R.string.a1_credits);
+                text[4].setText(R.string.a1_becarios);
+                text[5].setText(R.string.a1_aula);
+                break;
 
             case 1:
-
+                text[0].setText(R.string.a2);
+                text[1].setText(R.string.a2_professor);
+                text[2].setText(R.string.a2_detail);
+                text[3].setText(R.string.a2_credits);
+                text[4].setText(R.string.a2_becarios);
+                text[5].setText(R.string.a2_aula);
+                break;
 
             case 2:
-
+                text[0].setText(R.string.a3);
+                text[1].setText(R.string.a3_professor);
+                text[2].setText(R.string.a3_detail);
+                text[3].setText(R.string.a3_credits);
+                text[4].setText(R.string.a3_becarios);
+                text[5].setText(R.string.a3_aula);
+                break;
 
             case 3:
-
-
-            case 4:
-
+                text[0].setText(R.string.a4);
+                text[1].setText(R.string.a4_professor);
+                text[2].setText(R.string.a4_detail);
+                text[3].setText(R.string.a4_credits);
+                text[4].setText(R.string.a4_becarios);
+                text[5].setText(R.string.a4_aula);
+                break;
         }
     }
-
-
 }
