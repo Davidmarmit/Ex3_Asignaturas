b55887f04b571a8c4228bbd05d6c14b9edd416db
David <davidmartinezmit@gmail.com>
2022-03-05 17:45:58 +0100

    Afegim logos nous a drawable, strings actualitzats, Connexió entre les dues Activities, i començem a mostrar informacio en la segona activity. Modificacions de la main activity: afegim logo a cada assignatura.

--- app/src/main/java/com/lsasignaturas/MainActivity.java
+++ app/src/main/java/com/lsasignaturas/MainActivity.java
@@ -2,13 +2,31 @@ package com.lsasignaturas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private final LinearLayout[] boton = new LinearLayout[4];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton[0] = (LinearLayout) findViewById(R.id.b_a1);
+        boton[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra("asignatura",0);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/lsasignaturas/SecondActivity.java
+++ app/src/main/java/com/lsasignaturas/SecondActivity.java
@@ -1,4 +1,43 @@
 package com.lsasignaturas;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView[] text = new TextView[6];
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        int temp = intent.getIntExtra("asignatura",4);
+        text[0] = (TextView) findViewById(R.id.nom_assig);
+        text[1] = (TextView) findViewById(R.id.profesor);
+        text[2] = (TextView) findViewById(R.id.detalle);
+        text[3] = (TextView) findViewById(R.id.creditos);
+        text[4] = (TextView) findViewById(R.id.becarios);
+        text[5] = (TextView) findViewById(R.id.aula);
+
+        switch (temp){
+            case 0:
+
+
+            case 1:
+
+
+            case 2:
+
+
+            case 3:
+
+
+            case 4:
+
+        }
+    }
+
+
 }
